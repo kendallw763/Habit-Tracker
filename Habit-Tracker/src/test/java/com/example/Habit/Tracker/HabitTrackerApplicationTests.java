8c42e91fe9bfb11b9fe077ce8ad17b2d630d5cd3
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-22 12:59:49 -0400

    Add files via upload

---
+++ Habit-Tracker/src/test/java/com/example/Habit/Tracker/HabitTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Habit.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
