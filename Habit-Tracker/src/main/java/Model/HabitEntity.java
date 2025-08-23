13b18594470dffb189b260fb69f61d683b9cf61d
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-22 21:05:14 -0400

    Update HabitEntity.java

--- Habit-Tracker/src/main/java/Model/HabitEntity.java
+++ Habit-Tracker/src/main/java/Model/HabitEntity.java
@@ -1,4 +1,27 @@
 package Model;
 
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
 public class HabitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Getter @Setter
+    private Long id;
+    private String user, name, description;
+    private LocalDate createdAt;
+
+
+    @OneToMany(mappedBy = "HabitEntity", cascade = CascadeType.ALL)
+    List<HabitEntity> habitEntity;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne
+    private UserEntity userEntity;
 }
