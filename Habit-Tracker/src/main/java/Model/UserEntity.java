f863243f0f35fe1c0a1b8005ae8747d2a8129f96
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-22 20:39:26 -0400

    Update UserEntity.java
    This file contains the basic set up for the user entity class. Using Lombok import, the boilerplate code of getters and setters are eliminated using annotations (@Id, @Getter, @Setter). The fields are automatically structured to be used in future programming of this application. 
    
    @oneToMany specifies the ownership of the User Entity in a bilateral relationship. Starting with the 'User side'.
    
    (mapepdBy) - Talks to JPA/Hibernate and tells it how to create instances of the UserEntity class.

--- Habit-Tracker/src/main/java/Model/UserEntity.java
+++ Habit-Tracker/src/main/java/Model/UserEntity.java
@@ -1,4 +1,22 @@
 package Model;
 
+import java.util.List;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Getter
+    @Setter
+    private Long id;
+    private String username;
+    private String email;
+
+    @OneToMany(mappedBy = "UserEntity", cascade = CascadeType.ALL)
+    private List<UserEntity> userEntity;
 }
