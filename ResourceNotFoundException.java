36935f269c48a0ef9bd2d771dee5e6f32acb3731
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-25 20:32:00 -0400

    Redundant  ResourceNotFoundException.java

--- ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Habit.Tracker.Exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String theUserWasNotFound) {
-
-    }
-}
