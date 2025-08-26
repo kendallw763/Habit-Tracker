1c55ecc0eadc7e5fa58797aa2370573adbc98baf
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-25 20:35:23 -0400

    Update HabitService.java

--- Habit-Tracker/src/main/java/Service/HabitService.java
+++ Habit-Tracker/src/main/java/Service/HabitService.java
@@ -1,4 +1,22 @@
-package Service;
+package com.example.Habit.Tracker.Service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.example.Habit.Tracker.Model.HabitEntity;
+import com.example.Habit.Tracker.Repositories.HabitRepository;
+import com.example.Habit.Tracker.Exception.ResourceNotFoundException;
+
+@Service
 public class HabitService {
+
+    HabitRepository habitRepository;
+
+    public HabitService (HabitRepository habitRepository) {this.habitRepository = habitRepository;}
+
+    public HabitEntity createHabit(HabitEntity habitEntity){return habitRepository.save(habitEntity);}
+
+    public HabitEntity getHabitById(Long id){return habitRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Habit id not found"));}
+
+    public List<HabitEntity> getAllHabits(){return habitRepository.findAll();}
 }
