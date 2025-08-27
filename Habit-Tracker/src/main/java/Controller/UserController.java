ea46a842b1951c4e5f28ced586bc092c0b6c7de8
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-26 22:11:49 -0400

    Update UserController.java

--- Habit-Tracker/src/main/java/Controller/UserController.java
+++ Habit-Tracker/src/main/java/Controller/UserController.java
@@ -1,4 +1,38 @@
-package Controller;
+package com.example.Habit.Tracker.Controller;
 
+import org.springframework.http.ResponseEntity;
+import com.example.Habit.Tracker.Model.UserEntity;
+import com.example.Habit.Tracker.Service.UserService;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+    public UserController (UserService userService){
+        this.userService = userService;
+    }
+
+  @PostMapping
+    public UserEntity createUser(@RequestBody UserEntity user){
+            return userService.createUser(user);
+    }
+
+  @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable Long id){return userService.getUserById(id);}
+
+  @DeleteMapping("/users/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id){userService.deleteUserById(id);
+            return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public List<UserEntity>getAllUsers(){
+            return userService.getAllUsers();
+    }
 }
