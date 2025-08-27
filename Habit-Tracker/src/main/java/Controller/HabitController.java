bc212ec687b3df8690cae285618c5fa6cb2bd07f
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-26 22:10:55 -0400

    Update HabitController.java

--- Habit-Tracker/src/main/java/Controller/HabitController.java
+++ Habit-Tracker/src/main/java/Controller/HabitController.java
@@ -1,4 +1,36 @@
-package Controller;
+package com.example.Habit.Tracker.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.Habit.Tracker.Model.HabitEntity;
+import com.example.Habit.Tracker.Service.HabitService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/habits")
 public class HabitController {
+
+    public final HabitService habitService;
+
+    public HabitController (HabitService habitService){this.habitService = habitService;}
+
+    @PostMapping
+    public HabitEntity createHabit(@RequestBody HabitEntity habit){return habitService.createHabit(habit);}
+
+    @GetMapping("{id}")
+    public HabitEntity getHabitById (@PathVariable Long id){return habitService.getHabitById(id);}
+
+    @DeleteMapping("/users/{id}")
+        public ResponseEntity<Void> deleteHabitById (@PathVariable Long id){habitService.deleteHabitById(id);
+            return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping()
+    public List<HabitEntity>getAllHabit(){
+        return habitService.getAllHabits();
+    }
+
 }
