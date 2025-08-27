dc69a87ce955e6374b40217df1341ae40225143a
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-26 22:09:52 -0400

    Implemented delete function UserService.java

--- Habit-Tracker/src/main/java/Service/UserService.java
+++ Habit-Tracker/src/main/java/Service/UserService.java
@@ -18,5 +18,12 @@ public class UserService {
     public  UserEntity getUserById(Long id){return userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User id not found"));}
 
+    public void deleteUserById (Long id ){
+        if(!userRepository.existsById(id)){
+            throw new ResourceNotFoundException("User id not found");
+        }
+        userRepository.deleteById(id);
+    }
+
     public List<UserEntity> getAllUsers(){return userRepository.findAll();}
 }
