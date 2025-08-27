89f428ede06ca22c2db71978388c1414971b6905
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-08-26 22:11:22 -0400

    Update HabitLogController.java

--- Habit-Tracker/src/main/java/Controller/HabitLogController.java
+++ Habit-Tracker/src/main/java/Controller/HabitLogController.java
@@ -1,4 +1,35 @@
-package Controller;
+package com.example.Habit.Tracker.Controller;
 
+import com.example.Habit.Tracker.Model.HabitLogEntity;
+import com.example.Habit.Tracker.Service.HabitLogService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/habitLog")
 public class HabitLogController {
+
+   public final HabitLogService habitLogService;
+
+   public HabitLogController (HabitLogService habitLogService){this.habitLogService = habitLogService;}
+
+   @PostMapping
+        public HabitLogEntity createHabitLog(@RequestBody HabitLogEntity habitLog){
+            return habitLogService.createHabitLog(habitLog);}
+
+    @GetMapping("{id}")
+        public HabitLogEntity getHabitLogById (@PathVariable Long id){
+            return habitLogService.getHabitLogById(id);}
+
+    @DeleteMapping("/users/{id}")
+        public ResponseEntity<Void> deleteHabitLogById (@PathVariable Long id){
+            habitLogService.deleteHabitLogById(id);
+                return ResponseEntity.noContent().build();
+        }
+
+    @GetMapping()
+        public List<HabitLogEntity>getAllHabitLogs(){
+            return habitLogService.getAllHabitLogs();
+        }
 }
