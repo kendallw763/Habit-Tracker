11629af2e20628abb369f901f804d41df6d87654
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-09-02 08:04:06 -0400

    Update UserService.java

--- Habit-Tracker/src/main/java/Service/UserService.java
+++ Habit-Tracker/src/main/java/Service/UserService.java
@@ -1,29 +1,37 @@
 package com.example.Habit.Tracker.Service;
 
-import java.util.List;
-import org.springframework.stereotype.Service;
-import com.example.Habit.Tracker.Model.UserEntity;
+import com.example.Habit.Tracker.Model.User;
 import com.example.Habit.Tracker.Repositories.UserRepository;
 import com.example.Habit.Tracker.Exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 public class UserService {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    public UserService (UserRepository userRepository){this.userRepository = userRepository;}
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    public UserEntity createUser(UserEntity userEntity){return userRepository.save(userEntity);}
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
 
-    public  UserEntity getUserById(Long id){return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User id not found"));}
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
 
-    public void deleteUserById (Long id ){
-        if(!userRepository.existsById(id)){
-            throw new ResourceNotFoundException("User id not found");
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void deleteUserById(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new ResourceNotFoundException("User not found");
         }
         userRepository.deleteById(id);
     }
-
-    public List<UserEntity> getAllUsers(){return userRepository.findAll();}
 }
