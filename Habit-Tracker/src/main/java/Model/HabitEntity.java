0f9658fd09622bbfdf8fdb51d99d3f6bc4c302a6
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-09-02 07:55:22 -0400

    Update HabitEntity.java

--- Habit-Tracker/src/main/java/Model/HabitEntity.java
+++ Habit-Tracker/src/main/java/Model/HabitEntity.java
@@ -1,5 +1,28 @@
-package Model;
+package com.example.Habit.Tracker.Model;
 
-public class HabitEntity {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.List;
 
+@Entity
+@Getter @Setter
+public class Habit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String habit;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @OneToMany(mappedBy = "habit", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
+    private List<HabitLog> habitLogs;
 }
