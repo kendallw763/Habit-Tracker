be287bc77c8d104ce409aa07e7d235a7ef303a83
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-09-02 07:55:47 -0400

    Update HabitLogEntity.java

--- Habit-Tracker/src/main/java/Model/HabitLogEntity.java
+++ Habit-Tracker/src/main/java/Model/HabitLogEntity.java
@@ -1,5 +1,33 @@
-package Model;
+package com.example.Habit.Tracker.Model;
 
-public class HabitLogEntity {
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class HabitLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String note;
+
+    //@CreationTimestamp
+    //@Column(updatable = false, nullable = false)
+    //private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonBackReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "habit_id")
+    @JsonBackReference
+    private Habit habit;
 }
