c53e16df6dbba89b4ca0016e4f24e89210de5432
Kendall White <63067332+kendallw763@users.noreply.github.com>
2025-09-02 07:54:29 -0400

    Add files via upload

---
+++ Habit-Tracker/src/main/java/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.Habit.Tracker.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
